5228e7f18c7f6b611c5f4f121be73bdfdedd5066
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2019-02-09 18:36:23 -0500

    image size checker made

---
+++ image_size_checker.py
@@ -0,0 +1,16 @@
+import PIL
+from PIL import Image
+import os
+
+
+def size_check(file_dir):
+    im = Image.open(file_dir)
+    width, height = im.size
+    if width >= 1000 or height >= 1000:
+        print("Check: " + file_dir)
+
+
+
+training_dir = os.getcwd() + '\car'
+for filename in os.listdir(training_dir):
+    size_check(os.path.join(training_dir, filename))
