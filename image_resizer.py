5a7a99863e20b0e191df9ad57a214a1424929ac6
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2019-02-10 08:07:56 -0500

    Resizing and checking image sizes

--- image_resizer.py
+++ image_resizer.py
@@ -4,21 +4,29 @@ from PIL import Image
 
 def resize(filepath):
     basewidth = 500
-    img = Image.open(filepath)
-    wpercent = (basewidth / float(img.size[0]))
-    hsize = int((float(img.size[1]) * float(wpercent)))
-    img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
-    img.save('resized_image.jpg')
+    try:
+        img = Image.open(filepath)
+        wpercent = (basewidth / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+        img.save('resized_image.jpg')
+    except IOError:
+        print('image failure')
+        os.remove(filepath)
 
-    baseheight = 500
-    img = Image.open('resized_image.jpg')
-    hpercent = (baseheight / float(img.size[1]))
-    wsize = int((float(img.size[0]) * float(hpercent)))
-    img = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
-    img.save(filepath)
-    os.remove('resized_image.jpg')
+    try:
+        baseheight = 500
+        img = Image.open('resized_image.jpg')
+        hpercent = (baseheight / float(img.size[1]))
+        wsize = int((float(img.size[0]) * float(hpercent)))
+        img = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
+        img.save(filepath)
+        os.remove('resized_image.jpg')
+    except IOError:
+        print('image failure')
+        os.remove(filepath)
 
 
-training_dir = os.getcwd() + '\car'
+training_dir = os.getcwd() + '\\table'
 for filename in os.listdir(training_dir):
     resize(os.path.join(training_dir, filename))
--- image_size_checker.py
+++ image_size_checker.py
@@ -6,11 +6,11 @@ import os
 def size_check(file_dir):
     im = Image.open(file_dir)
     width, height = im.size
-    if width >= 1000 or height >= 1000:
+    if width > 1000 or height > 1000:
         print("Check: " + file_dir)
 
 
 
-training_dir = os.getcwd() + '\car'
+training_dir = os.getcwd() + '\\table'
 for filename in os.listdir(training_dir):
     size_check(os.path.join(training_dir, filename))
